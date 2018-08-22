d72e1a824d4c430c91ca42a9abb3002781c15274
Schlueter <SchlueterPro@gmail.com>
2018-08-21 18:20:44 -0700

    point save

--- Bamazon.js
+++ Bamazon.js
@@ -95,7 +95,7 @@ function start() {
   inquirer
     .prompt({
       name: "pickAnItem",
-      type: "rawlist",
+      type: "list",
       message: "Which product would you like to purchase?",
       choices: ["action_figure", "bicycle", "baseball_bat", "scooter", "puzzle", "drone", "tent", "fishing_pole", "blender", "slinky"]
     })
@@ -165,70 +165,5 @@ function purchaseItem() {
 function outOfStock() {
     console.log("Insufficient Quantity!");
     start();
-
-
-
-
-
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
-
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
 }
 
